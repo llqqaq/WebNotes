3cb54fb16fa6ef849bbfa9b3f82b25e11de524b8
llqqaq <1181707441@qq.com>
2021-11-01 18:31:25 +0800

    save

---
+++ webpack/03-webpack打包html资源/webpack.config.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * loader: 1.下载 2.使用(配置loader)
+ * plugins: 1.下载 2.引入 3.使用
+ */
+
+
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './index.js',
+
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build')
+    },
+
+    module: {
+        rules: [
+            // loader的配置
+        ]
+    },
+
+    plugins: [
+        // plugins的配置
+
+        // html-webpack-plugin
+        // 功能： 默认会创建一个空的html，自动引入打包输出的所有资源（js/css）
+        // 需求: 需要有结构的html文件
+        new HtmlWebpackPlugin({
+            // 复制'./index.html' 文件 并自动引入打包输出的所有资源（js/css）
+            template: './index.html'
+        })
+    ],
+    mode: 'development'
+}
---
+++ webpack/04-webpack打包图片资源/webpack.config.js
@@ -0,0 +1,58 @@
+const {
+    resolve
+} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './index.js',
+
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build')
+    },
+
+    module: {
+        rules: [{
+                test: /\.less$/,
+                // 使用多个loader写法
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                // 问题：默认处理不了html中img图片  
+                // 处理图片资源
+                test: /\.(jpg|png|gif)$/,
+                // 使用一个loader
+                // 下载 url-loader file-loader
+                loader: 'url-loader',
+                options: {
+                    // 图片大小小于369kb，就会被base64处理
+                    // 优点：减少请求数量（减轻服务器压力）
+                    // 缺点：图片体积会变更大（文件请求速度更慢）
+                    limit: 369 * 1024,
+                    // 问题：因为url-loader默认使用es6模块化解析，而html-loader引入图片是common.js
+                    // 解析时会出现问题：src路径变为[object Module]
+                    // 解决：关闭url-loader的es6模块化，使用commonjs解析
+                    esModule: false,
+                    // 给图片进行重命名
+                    // [hash: 10]取图片的hash的前10位
+                    // [ext]取文件原来拓展名
+                    name: '[hash:10].[ext]'
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+
+    mode: 'development'
+}
